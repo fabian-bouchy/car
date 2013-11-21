30f428d382e897e7ffb3f68d8630e43f60314731
Fabian BOUCHY <fabian.bouchy@gmail.com>
2013-11-21 11:55:36 +0100

    Remove warning and merge

--- src/common/ConfigManager.java
+++ src/common/ConfigManager.java
@@ -1,10 +1,7 @@
 package common;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
