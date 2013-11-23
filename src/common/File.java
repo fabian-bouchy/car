d7e73b712471ac27e47c28366d949334f3203f7a
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2013-11-23 18:00:26 +0100

    File constructor

--- src/common/File.java
+++ src/common/File.java
@@ -1,5 +1,44 @@
 package common;
 
-public class File {
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
+public class File {
+	
+	private String name;
+	private String id;
+	private byte[] data;
+	
+	public File(String name, String id, String fileName) throws IOException{
+		// fill in fields
+		this.name = name;
+		this.id = id;
+		// read the file
+		RandomAccessFile f = new RandomAccessFile(fileName, "r");
+		this.data = new byte[(int)f.length()];
+		f.read(this.data);
+		f.close();
+	}
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public byte[] getData() {
+		return data;
+	}
+	public void setData(byte[] data) {
+		this.data = data;
+	}
 }
