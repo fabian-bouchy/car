01f273326d2ff5ad6374b6bea1cda1131f79b4dd
Fabian BOUCHY <fabian.bouchy@gmail.com>
2013-12-08 16:32:07 +0100

    Comment the file RemoteServer

--- src/client/RemoteServer.java
+++ src/client/RemoteServer.java
@@ -13,10 +13,7 @@ import common.RemoteNode;
 import common.UtilBobby;
 
 /**
- *	@author mickey
- *	
  *	High-level representation of a replica server
- *
  */
 public class RemoteServer extends RemoteNode {
 
@@ -24,6 +21,9 @@ public class RemoteServer extends RemoteNode {
 		super(sName, sIpAddress, iPriority, iPort);
 	}
 
+	/**
+	 * Start connection and implement the protocol "write" with the thread ThreadWrite.
+	 */
 	@Override
 	public void write(File file) throws UnknownHostException, IOException {
 		System.out.println("[remote server] writing " + file);
@@ -31,15 +31,18 @@ public class RemoteServer extends RemoteNode {
 		Socket socketToServer  = this.connect();
 		ObjectOutputStream out = new ObjectOutputStream(socketToServer.getOutputStream());                   
 		ObjectInputStream  in  = new ObjectInputStream(socketToServer.getInputStream());
-        
+
+		// Init client write connection
         out.writeObject(UtilBobby.CLIENT_WRITE);
         String answer;
 		try {
 			answer = (String) in.readObject();
+			// Be sure that the server is ready.
 			if(answer.equals(UtilBobby.SERVER_WRITE_READY)) {
-				
+				// Send the file to the server.
 				out.writeObject(file);
 				answer = (String) in.readObject();
+				// Check result return by the server.
 				if(answer.equals(UtilBobby.SERVER_WRITE_OK)){
 					System.out.println("[remote server] Write done!");
 				}else{
@@ -47,33 +50,31 @@ public class RemoteServer extends RemoteNode {
 				}
 			}
 		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
+			System.out.println("[remote server] write failed: " + e.getLocalizedMessage());
 		}
-        
 	}
 
+	/**
+	 * Implment the protocol "delete" initiate with the thread ThreadDelete.
+	 */
 	@Override
-	public boolean has(File id) throws UnknownHostException, IOException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean delete(File file) throws UnknownHostException, IOException {
+	public boolean delete(File metadata) throws UnknownHostException, IOException {
 		// Initialize the connection:
 		Socket socketToServer = this.connect();
 		ObjectOutputStream out = new ObjectOutputStream(socketToServer.getOutputStream());                   
 		ObjectInputStream  in  = new ObjectInputStream(socketToServer.getInputStream());
 
+		// Initiate client delete session
         out.writeObject(UtilBobby.CLIENT_DELETE);
         String answer;
 		try {
 			answer = (String) in.readObject();
+			// Check if server is ready to receive the metadata to identify the file to delete it.
 			if(answer.equals(UtilBobby.SERVER_DELETE_READY)) {
-				
-				out.writeObject(file);
+				// Send metadata data
+				out.writeObject(metadata);
 				answer = (String) in.readObject();
-				
+				// Check if delete succeed
 				if(answer.equals(UtilBobby.SERVER_DELETE_OK)){
 					System.out.println("[remote server] Delete done!");
 				}else{
@@ -82,12 +83,14 @@ public class RemoteServer extends RemoteNode {
 			}
 			return false;
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("[remote server] delete failed: " + e.getLocalizedMessage());
 		}
 		return false;
 	}
 
+	/**
+	 * Implment the protocol "read" initiate with the thread ThreadRead. 
+	 */
 	@Override
 	public File read(File metadata) throws Exception {
 		System.out.println("[remote server] read init");
@@ -96,12 +99,14 @@ public class RemoteServer extends RemoteNode {
 		ObjectOutputStream out = new ObjectOutputStream(socketToServer.getOutputStream());                   
 		ObjectInputStream  in  = new ObjectInputStream(socketToServer.getInputStream());
 
+		// Initiate read session
         out.writeObject(UtilBobby.CLIENT_READ);
         String answer = (String) in.readObject();
         
         if(answer.equals(UtilBobby.SERVER_READ_READY)) {
         	System.out.println("[remote server] ready to read from " + this);
-        	
+
+        	// Send metadata to identify the request file
         	out.writeObject(metadata);
 			answer = (String) in.readObject();
 			
@@ -109,12 +114,12 @@ public class RemoteServer extends RemoteNode {
 				// Read file from server
 				File tmp = (File) in.readObject();
 				if(tmp != null ) {
-					System.out.println("[remote server] File read "+tmp);
+					System.out.println("[remote server] File read " + tmp);
 					return tmp;
 				}
 			}
 			// TODO Change this stack overflow possible on read function...
-			// Internal redirection
+			// Internal redirection in case the remote server doesn't own the file.
 			else if(answer.contains(UtilBobby.SERVER_READ_REDIRECT_TO) && (answer.split(UtilBobby.SPLIT_REGEX).length == 4)){
 				String nextHopName = answer.split(UtilBobby.SPLIT_REGEX)[3];
 				RemoteNode nextHop =  ConfigManager.getRemoteNode(nextHopName);
@@ -138,24 +143,9 @@ public class RemoteServer extends RemoteNode {
 		return "[remote server] "+this.getName()+" ("+this.getPriority()+") @ "+this.getIpAddress()+":"+this.getPort();
 	}
 
-	@Override
-	public HashMap<String, File> getMetadata() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean commitWrite(File file) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean abortWrite(File file) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	/**
+	 * Retreive all file for a specific usename.
+	 */
 	@Override
 	public HashMap<String, File> listFiles(String username) throws Exception {
 		System.out.println("[remote server] listing files for " + username);
@@ -179,5 +169,28 @@ public class RemoteServer extends RemoteNode {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	/**
+	 * Function not yet necessary in this context.
+	 */
+	@Override
+	public boolean has(File id) throws UnknownHostException, IOException {
+		return false;
+	}
+
+	@Override
+	public HashMap<String, File> getMetadata() throws Exception {
+		return null;
+	}
+
+	@Override
+	public boolean commitWrite(File file) throws Exception {
+		return false;
+	}
+
+	@Override
+	public boolean abortWrite(File file) throws Exception {
+		return false;
 	}
 }
