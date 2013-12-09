a5e5c9c042a949e65412254aafbc94a0a89a109b
Fabian BOUCHY <fabian.bouchy@gmail.com>
2013-12-09 10:25:05 +0100

    Add return message when RemoteReplica failed

--- src/server/thread/ThreadReplicaServer.java
+++ src/server/thread/ThreadReplicaServer.java
@@ -54,26 +54,30 @@ public class ThreadReplicaServer extends ThreadWorker{
 				
 				// send all meta-data from the node: the ones I have and the ones I don't have
 				if (cmd[1].equals(UtilBobby.REPLICA_METADATA_SYMBOL)) {
-					
-					if (cmd[2].equals(UtilBobby.REPLICA_METADATA_GET_SYMBOL)) {
-						
-						out.writeObject(UtilBobby.REPLICA_METADATA_READY);
-						System.out.println("[thread replica server] get metadata ready");
-						
-						// external meta-data
-						HashMap<String, File> metadata = FileManager.getMetadata();
-						// internal meta-data
-						for (File file : FileManager.getFiles().values()){
-							metadata.put(file.getId(), file.generateMetadata());
+					try {
+						if (cmd[2].equals(UtilBobby.REPLICA_METADATA_GET_SYMBOL)) {
+
+							out.writeObject(UtilBobby.REPLICA_METADATA_READY);
+							System.out.println("[thread replica server] get metadata ready");
+
+							// external meta-data
+							HashMap<String, File> metadata = FileManager.getMetadata();
+							// internal meta-data
+							for (File file : FileManager.getFiles().values()){
+								metadata.put(file.getId(), file.generateMetadata());
+							}
+
+							out.writeObject(metadata);
+
+							if (((String)in.readObject()).equals(UtilBobby.REPLICA_METADATA_OK)){
+								System.out.println("[thread replica server] metadata sent!");
+							}else{
+								System.out.println("[thread replica server] metadata not sent!");
+							}						
 						}
-						
-						out.writeObject(metadata);
-						
-						if (((String)in.readObject()).equals(UtilBobby.REPLICA_METADATA_OK)){
-							System.out.println("[thread replica server] metadata sent!");
-						}else{
-							System.out.println("[thread replica server] metadata not sent!");
-						}						
+					} catch (Exception e) {
+						out.writeObject(UtilBobby.REPLICA_METADATA_KO);
+						System.out.println("[thread replica server] metadata failed! " + e.getLocalizedMessage());
 					}
 				}
 
@@ -81,110 +85,97 @@ public class ThreadReplicaServer extends ThreadWorker{
  *	----------------------------------------------------------------------------------------------------------------------
  *	WRITE
  */
-				
+
 				// store the files/metadata we receive
 				if(cmd[1].equals(UtilBobby.REPLICA_WRITE_SYMBOL)) {
-					
-					System.out.println("[thread replica server] write");
-					
-					// check if we have the file
-					File have = FileManager.getFile(cmd[2]);
-					File metadata = FileManager.getMetadata(cmd[2]);
-					// send a "ready" message
-					if (have != null || metadata == null){
-						out.writeObject(UtilBobby.REPLICA_WRITE_READY_FILE);
-					}else{
-						out.writeObject(UtilBobby.REPLICA_WRITE_READY_META);
-					}
-					// receive the file
-					File file = (File) in.readObject();
-					
-					System.out.println("[thread replica server] file received: "+file);
-					
-					if (file.isFile()){
-						System.out.println("[thread replica server] is a file");
-						// let's verify if there is a conflict
-						File oldFile = FileManager.getFileOrMetadata(file.getId());
+					try {
 
-						if (oldFile == null)
-						{
-							// check if it's not already in the temporary storage waiting for a commit
-							// new file
-							System.out.println("[thread replica server] new file");
-							FileManager.prepare(file); // store in temporary storage
-							out.writeObject(UtilBobby.REPLICA_WRITE_OK);
-							System.out.println("[thread replica server] file prepared");
+						System.out.println("[thread replica server] write");
+
+						// check if we have the file
+						File have = FileManager.getFile(cmd[2]);
+						File metadata = FileManager.getMetadata(cmd[2]);
+						// send a "ready" message
+						if (have != null || metadata == null){
+							out.writeObject(UtilBobby.REPLICA_WRITE_READY_FILE);
+						} else {
+							out.writeObject(UtilBobby.REPLICA_WRITE_READY_META);
 						}
-						else 
-						{
-							System.out.println("[thread replica server] update");
-							// TODO acquire a lock on the old file ?
-							try{
+						// receive the file
+						File file = (File) in.readObject();
+
+						System.out.println("[thread replica server] file received: "+file);
+
+						if (file.isFile()) {
+							System.out.println("[thread replica server] is a file");
+							// let's verify if there is a conflict
+							File oldFile = FileManager.getFileOrMetadata(file.getId());
+
+							if (oldFile == null) {
+								// check if it's not already in the temporary storage waiting for a commit
+								// new file
+								System.out.println("[thread replica server] new file");
+								FileManager.prepare(file); // store in temporary storage
+								out.writeObject(UtilBobby.REPLICA_WRITE_OK);
+								System.out.println("[thread replica server] file prepared");
+							} else {
+								System.out.println("[thread replica server] update");
+								// TODO acquire a lock on the old file ?
 								System.out.println("[thread replica server] acquiring a lock on : "+oldFile);
 								oldFile.lock();
 								System.out.println("[thread replica server] lock acquired on : "+oldFile);
-							}catch(InterruptedException e){
-								e.printStackTrace();
-							}
-							
-							System.out.println("[thread replica server] comp:" + oldFile.isCompatibleWith(file) + " new:" + file.getGlobalVersion() + " old+1:" + (oldFile.getGlobalVersion() + 1));
-							
-							if (oldFile.isCompatibleWith(file) && (file.getGlobalVersion() >= (oldFile.getGlobalVersion() + 1)))
-							{
-								// not new, but no conflict
-								// TODO verify conditions and locks
-								// TODO unlock in commit
-								FileManager.prepare(file); // store in temporary storage
-								out.writeObject(UtilBobby.REPLICA_WRITE_OK);
-								
-								try {
-									oldFile.unlock();
-									System.out.println("[thread replica server] released the lock on : "+oldFile);
-								} catch (InterruptedException e) {
-									e.printStackTrace();
-								}
-							}
-							else
-							{
-								// there is a conflicted copy
-								int myPriority = ConfigManager.getMe().getPriority();
-								int theirPriority = Integer.MIN_VALUE;
-								RemoteNode them = ConfigManager.getRemoteNodeByIp(clientSocket.getInetAddress().getHostAddress());
-								if (them != null){
-									theirPriority = them.getPriority();
-								}
-								
-								System.out.println("[thread replica server] my priority is " + myPriority + " theirs is " +theirPriority);
-								
-								if (myPriority < theirPriority){
-									// TODO verify this is correct
-									System.out.println("[thread replica server] I obey and store the new file");
-									FileManager.addOrReplaceFile(file);
+
+								System.out.println("[thread replica server] comp:" + oldFile.isCompatibleWith(file) + " new:" + file.getGlobalVersion() + " old+1:" + (oldFile.getGlobalVersion() + 1));
+
+								if (oldFile.isCompatibleWith(file) && (file.getGlobalVersion() >= (oldFile.getGlobalVersion() + 1)))
+								{
+									// not new, but no conflict
+									// TODO verify conditions and locks
+									// TODO unlock in commit
+									FileManager.prepare(file); // store in temporary storage
 									out.writeObject(UtilBobby.REPLICA_WRITE_OK);
-								}else{
-									// reject the file
-									System.out.println("[thread replica server] I refuse the new file");
-									out.writeObject(UtilBobby.REPLICA_WRITE_KO);
-								}
-								
-								try {
+
 									oldFile.unlock();
 									System.out.println("[thread replica server] released the lock on : "+oldFile);
-								} catch (InterruptedException e) {
-									e.printStackTrace();
+								} else {
+									// there is a conflicted copy
+									int myPriority = ConfigManager.getMe().getPriority();
+									int theirPriority = Integer.MIN_VALUE;
+									RemoteNode them = ConfigManager.getRemoteNodeByIp(clientSocket.getInetAddress().getHostAddress());
+									if (them != null){
+										theirPriority = them.getPriority();
+									}
+
+									System.out.println("[thread replica server] my priority is " + myPriority + " theirs is " +theirPriority);
+
+									if (myPriority < theirPriority){
+										// TODO verify this is correct
+										System.out.println("[thread replica server] I obey and store the new file");
+										FileManager.addOrReplaceFile(file);
+										out.writeObject(UtilBobby.REPLICA_WRITE_OK);
+									} else {
+										// reject the file
+										System.out.println("[thread replica server] I refuse the new file");
+										out.writeObject(UtilBobby.REPLICA_WRITE_KO);
+									}
+
+									oldFile.unlock();
+									System.out.println("[thread replica server] released the lock on : "+oldFile);
 								}
 							}
-							
+						} else {
+							System.out.println("[thread replica server] metadata received "+file);
+							// it's a meta-data file, no content
+							// we just store the new value and say we're good
+							file.setHasFile(false);
+							FileManager.prepare(file); // store in temporary storage
+							out.writeObject(UtilBobby.REPLICA_WRITE_OK);
 						}
-						
-					}else{
-						System.out.println("[thread replica server] metadata received "+file);
-						// it's a meta-data file, no content
-						// we just store the new value and say we're good
-						file.setHasFile(false);
-						FileManager.prepare(file); // store in temporary storage
-						out.writeObject(UtilBobby.REPLICA_WRITE_OK);
+					} catch (Exception e) {
+						System.out.println("[thread replica server] failed! " + e.getLocalizedMessage());
+						out.writeObject(UtilBobby.REPLICA_WRITE_KO);
 					}
+					
 				}
 				
 /*
@@ -258,8 +249,7 @@ public class ThreadReplicaServer extends ThreadWorker{
 				
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
+			System.out.println("[thread replica server] failed, details: ");
 			e.printStackTrace();
 		}
 		System.out.println("[thread replica server] end");
